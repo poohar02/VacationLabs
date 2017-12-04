bc4375b7dd7e307004e9708deb55b5d476fa0ee1
poohar02 <34246206+poohar02@users.noreply.github.com>
2017-12-04 23:00:31 +0530

    Update prog.js

--- prog.js
+++ prog.js
@@ -1,32 +1,31 @@
-	for(var i=1; i<=100;++i)
+for(var i=1; i<=100;++i)
+{
+	if(i%3==0)
 	{
-		if(i%3==0)
+		document.write("<br/>Earth");
+		if(i%5==0)
 		{
-			document.write("<br/>Earth");
-			if(i%5==0)
-			{
-				document.write(",Jupiter");
-			}
-			if(i%9==0)
-			{
-				document.write(",Pluto");
-			}
+			document.write(",Jupiter");
 		}
-		else if(i%5==0)
+		if(i%9==0)
 		{
-			document.write("<br/>Jupiter");
-			if(i%9==0)
-			{
-				document.write(",Pluto");
-			}
+			document.write(",Pluto");
 		}
-		else if(i%9==0)
-		{
-			document.write("<br/>Pluto");
-		}
-		else 
-		{
-			document.write("<br/>"+i);
-		}
-
 	}
+	else if(i%5==0)
+	{
+		document.write("<br/>Jupiter");
+		if(i%9==0)
+		{
+			document.write(",Pluto");
+		}
+	}
+	else if(i%9==0)
+	{
+		document.write("<br/>Pluto");
+	}
+	else 
+	{
+		document.write("<br/>"+i);
+	}
+}
