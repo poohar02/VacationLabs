12cfc8d5543c1faa87b05c985add460c170fe84f
poohar02 <34246206+poohar02@users.noreply.github.com>
2017-12-04 22:53:32 +0530

    Add files via upload

---
+++ prog.js
@@ -0,0 +1,32 @@
+	for(var i=1; i<=100;++i)
+	{
+		if(i%3==0)
+		{
+			document.write("<br/>Earth");
+			if(i%5==0)
+			{
+				document.write(",Jupiter");
+			}
+			if(i%9==0)
+			{
+				document.write(",Pluto");
+			}
+		}
+		else if(i%5==0)
+		{
+			document.write("<br/>Jupiter");
+			if(i%9==0)
+			{
+				document.write(",Pluto");
+			}
+		}
+		else if(i%9==0)
+		{
+			document.write("<br/>Pluto");
+		}
+		else 
+		{
+			document.write("<br/>"+i);
+		}
+
+	}
